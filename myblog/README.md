4fc055c4b908240b7109e0fb3fae08782043dcee
Julia Langoni <julia.langoni@gmail.com>
2025-03-25 20:47:32 -0300

    Actualicé hasta gitignore

---
+++ myblog/README.md
@@ -0,0 +1,2 @@
+# miblog
+Proyecto de blog coderhouse
---
+++ myblog/myblog/blog/urls.py
@@ -0,0 +1,19 @@
+
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.urls import path, include
+ 
+urlpatterns = [
+     path('admin/', admin.site.urls),
+     path('', views.home, name='home'),
+     path('agregar_transporte/', views.agregar_transporte, name='agregar_transporte'),
+     path('agregar_alojamiento/', views.agregar_alojamiento, name='agregar_alojamiento'),
+     path('agregar_actividades/', views.agregar_actividades, name='agregar_actividades'),
+     path('buscar/', views.buscar, name='buscar'),
+     path('admin/', admin.site.urls),
+     path('blog/', include('blog.urls')),
+ ]
+
+
+
